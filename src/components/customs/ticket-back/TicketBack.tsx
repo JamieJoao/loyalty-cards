7de06fb4ac9147284826ff43002c4a7866f53ba7
JamieJoao <jojham1994@gmail.com>
2023-09-02 15:18:17 -0500

    feat: new design for back card

--- src/components/customs/ticket-back/TicketBack.tsx
+++ src/components/customs/ticket-back/TicketBack.tsx
@@ -1,8 +1,12 @@
 import { FC } from 'react'
 import cn from 'classnames'
 
-import CakeImage from 'assets/images/cake-2.png'
 import { CardBack } from 'src/types/EnviromentsInterface'
+
+import Pow1Image from 'assets/images/pow-1.png'
+import Pow2Image from 'assets/images/pow-2.png'
+import HeartImage from 'assets/images/heart.svg'
+import CupcakeImage from 'assets/images/cupcake.png'
 
 interface TicketBackProps {
   cardBackData: CardBack
@@ -14,7 +18,7 @@ export const TicketBack: FC<TicketBackProps> = ({
   stage = 1
 }) => (
   <section className="bc-card --back">
-    <div className="bc-card__title">
+    {/* <div className="bc-card__title">
       <span>{title}</span>
       <span>{subtitle}</span>
     </div>
@@ -39,12 +43,48 @@ export const TicketBack: FC<TicketBackProps> = ({
                 <span>{obj === 0 ? '' : `${obj}%`}</span>
               </div>
             )}
+        </div>
+      ))}
+    </div> */}
 
-          {/* <div className='bc-card__square-value'>
-            <span>{obj === 0 ? index + 1 : `${obj}`}</span>
-          </div> */}
+    <div className="bc-card__background">
+      <img className='bc-card__background-cupcake' src={Pow2Image} alt='imagen de circulos' />
+      <img className='bc-card__background-cupcake' src={Pow2Image} alt='imagen de circulos' />
+      <img className='bc-card__background-cupcake' src={Pow1Image} alt='imagen de circulos' />
+      <img className='bc-card__background-cupcake' src={Pow1Image} alt='imagen de circulos' />
+    </div>
+
+    <div className="bc-card__texts">
+      <div className='bc-card__texts-title'>
+        <img src={HeartImage} alt="imagen de corazón" />
+        <span>{title}</span>
+        <img src={HeartImage} alt="imagen de corazón" />
+      </div>
+      <p className='bc-card__texts-subtitle'>{subtitle}</p>
+    </div>
+
+    <div className="bc-card__space-container">
+      {gifts.map((obj, index) => (
+        <div
+          key={index}
+          className={cn(
+            'bc-card__space',
+            obj > 0 && '--discount'
+          )}>
+          {index < stage
+            ? (
+              <img
+                className='bc-card__space-cupcake'
+                src={CupcakeImage}
+                alt='imagen de cupcake' />
+            )
+            : obj === 100
+              ? <span>FREE</span>
+              : obj > 0 && <span>{obj}%</span>
+          }
         </div>
       ))}
     </div>
+
   </section>
 )
